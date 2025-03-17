8bf9babbfc93756bc81ad155a131e8f2a3304b47
Joan González <joangonzalezalbert@gmail.com>
2025-03-17 22:26:08 +0100

    Install required dependecies

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const marked = require("marked")
+import { marked } from "marked";
+import { readFile, writeFile, mkdir, cp } from "fs/promises";
+import Handlebars from "handlebars";
+import { existsSync } from "fs";
 
+async function mdToHtml() {
+    const news = await readFile("./page/md/news.md", "utf8");
 
-function main() {
-    console.log("compile");
-
+    return {
+        news: marked(news)
+    }
 }
 
-main()
+async function insertInfoHtml(obj) {
+    const html = await readFile("./page/template.html")
+    const result = Handlebars.compile(html.toString())(obj)
+    return result
+}
+
+async function main() {
+    try {
+
+        const mdObjStrings = await mdToHtml()
+
+        const newHtml = await insertInfoHtml(mdObjStrings);
+
+        await writeFile("./dist/index.html", newHtml)
+        await cp("./page/style.css", "./dist/style.css")
+
+    } catch (error) {
+        console.error("Error al leer el archivo:", error);
+    }
+}
+
+main();
---
+++ page/md/news.md
@@ -0,0 +1,5 @@
+# Hola mundo
+
+Este es el primer post de las news
+
+![Holaa mundo](https://https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FApache_License&psig=AOvVaw0kJWL6vCwe5ZZEu5-qzY-p&ust=1742335099365000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMiH9NqAkowDFQAAAAAdAAAAABAE)
